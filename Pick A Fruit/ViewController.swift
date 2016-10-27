960aa7d4fe1d58c9b66d63892f27d2a843a88d64
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-10-27 12:57:04 +0300

    Building the PickerView

--- Pick A Fruit/ViewController.swift
+++ Pick A Fruit/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate {
+    
+    var fruits = ["Apples", "Oranges", "Lemons", "Limes", "Blueberries"]
 
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    // UIPickerViewDelegate:
+    //
+    // returns the number of 'columns' to display.
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // UIPickerViewDelegate:
+    //
+    // returns the # of rows in each component..
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruits.count
+    }
+    
+    // UIPickerViewDelegate:
+    //
+    // returns title for the row.
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruits[row]
+    }
+} 
 
