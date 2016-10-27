af2b01875fd0bb894d76599638363d7d0c871df0
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-10-27 14:22:09 +0300

    Add images

--- Pick A Fruit/ViewController.swift
+++ Pick A Fruit/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate {
     
-    var fruits = ["Apples", "Oranges", "Lemons", "Limes", "Blueberries"]
+    var fruits = ["Make a choice", "Apples", "Oranges", "Lemons", "Limes", "Blueberries"]
 
     @IBOutlet var imageView: UIImageView!
     
@@ -19,6 +19,8 @@ class ViewController: UIViewController, UIPickerViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        imageView.image = UIImage(named: "fruits.jpg")
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,5 +48,31 @@ class ViewController: UIViewController, UIPickerViewDelegate {
     public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return fruits[row]
     }
-} 
+
+    // UIPickerViewDelegate:
+    //
+    // observe event - did select row
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch (fruits[row]) {
+            case "Apples":
+                imageView.image = UIImage(named: "apples.jpg")
+                infoLabel.text = "Apples"
+            case "Oranges":
+                imageView.image = UIImage(named: "oranges.jpg")
+                infoLabel.text = "Oranges"
+            case "Lemons":
+                imageView.image = UIImage(named: "lemons.jpg")
+                infoLabel.text = "Lemons"
+            case "Limes":
+                imageView.image = UIImage(named: "limes.jpg")
+                infoLabel.text = "Limes"
+            case "Blueberries":
+                imageView.image = UIImage(named: "blueberries.jpg")
+                infoLabel.text = "Blueberries"
+            default:
+                imageView.image = UIImage(named: "fruits.jpg")
+                infoLabel.text = "Please select a fruit."
+        }
+    }
+}
 
